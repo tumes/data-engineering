7d791071aa925a48e4bb32e22632d582aceae019
Robert Brandin <robert.brandin@factorylabs.com>
2014-03-11 15:32:47 -0600

    PurchaseOrders create LineItems

--- challenge/app/models/purchase_order.rb
+++ challenge/app/models/purchase_order.rb
@@ -2,4 +2,18 @@ class PurchaseOrder < ActiveRecord::Base
 
   has_many :line_items
 
+  attr_writer :line_item_data
+
+  before_create :create_line_items
+
+  private
+
+  def create_line_items
+    @line_item_data ||= []
+
+    @line_item_data.each do |li|
+      LineItem.create(purchaser_name: li[0], item_description: li[1], item_price: li[2], purchase_count: li[3], merchant_address: li[4], merchant_name: li[5])
+    end
+  end
+
 end
--- challenge/spec/models/purchase_order_spec.rb
+++ challenge/spec/models/purchase_order_spec.rb
@@ -4,4 +4,15 @@ describe PurchaseOrder do
 
   it { should have_many(:line_items) }
 
+  describe '.create' do
+    let(:line_item_data_1) { ["Guy Customer", "$5 for $2.50", "2.50", "2", "1234 Street Pl", "Item Barn"] }
+    let(:line_item_data_2) { ["Polly Customer", "$8 for $4", "4", "1", "1234 Place St", "Stuff Gazebo"] }
+    let(:purchase_order) { PurchaseOrder.new(line_item_data: [line_item_data_1, line_item_data_2]) }
+
+    it 'creates a line item for each chunk of data passed in' do
+      expect(LineItem).to receive(:create).twice
+      purchase_order.save
+    end
+  end
+
 end
