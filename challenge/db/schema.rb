3773f878a8d412d5dce556eee24cfc5588643388
Robert Brandin <robert.brandin@factorylabs.com>
2014-03-11 13:37:31 -0600

    Add Purchaser model

--- challenge/db/schema.rb
+++ challenge/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311192738) do
+ActiveRecord::Schema.define(version: 20140311193617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20140311192738) do
   add_index "line_items", ["purchase_order_id"], name: "index_line_items_on_purchase_order_id", using: :btree
 
   create_table "purchase_orders", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "purchasers", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
