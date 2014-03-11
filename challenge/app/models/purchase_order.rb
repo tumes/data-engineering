9f543241aafddc9cdeb1ec4745cf3cd80b5949b7
Robert Brandin <robert.brandin@factorylabs.com>
2014-03-11 17:44:24 -0600

    Documentation and housekeeping

--- challenge/app/models/purchase_order.rb
+++ challenge/app/models/purchase_order.rb
@@ -15,6 +15,8 @@ class PurchaseOrder < ActiveRecord::Base
   def create_line_items
     @line_item_data ||= []
 
+    # The line item data is fed to this method as an array of arrays that
+    # describe a split row of a tab-delimited file.
     @line_item_data.each do |li|
       LineItem.create(purchase_order: self, purchaser_name: li[0], item_description: li[1], item_price: li[2], purchase_count: li[3], merchant_address: li[4], merchant_name: li[5])
     end
