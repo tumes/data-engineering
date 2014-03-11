85904c0dfe85b9a05c0357bd28bae201a2863d61
Robert Brandin <robert.brandin@factorylabs.com>
2014-03-11 13:01:36 -0600

    Initial Rails app commit

---
+++ challenge/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Challenge::Application.config.secret_key_base = 'cd65ffd7cf20778f5d4ea8bf20c3526f3a49df2348e6327b39e56bfa6c97c2e2ee4fba102c859267307904742199359d93f4d6865b7e48376e561de30cad632b'
