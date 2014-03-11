486082c65cbab115300a546f34df804d9353eba6
Robert Brandin <robert.brandin@factorylabs.com>
2014-03-11 13:40:41 -0600

    Add Merchant model

--- challenge/db/schema.rb
+++ challenge/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311193849) do
+ActiveRecord::Schema.define(version: 20140311194001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20140311193849) do
   end
 
   add_index "line_items", ["purchase_order_id"], name: "index_line_items_on_purchase_order_id", using: :btree
+
+  create_table "merchants", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "purchase_orders", force: true do |t|
     t.datetime "created_at"
